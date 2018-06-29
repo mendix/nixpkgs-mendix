2d09a72311db4245eae74d290048ed03069e9396
Sander van der Burg <sander.van.der.burg@mendix.com>
2018-06-29 16:16:36 +0200

    Fix overriding buildInputs

--- build-support/package-mendix-app/default.nix
+++ build-support/package-mendix-app/default.nix
@@ -1,15 +1,16 @@
 {stdenv, mxbuild, jdk, nodejs}:
-{name, mendixVersion, looseVersionCheck ? false, ...}@args:
+{name, mendixVersion, looseVersionCheck ? false, buildInputs ? [], ...}@args:
 
 let
   mxbuildPkg = mxbuild."${mendixVersion}";
+  extraArgs = removeAttrs args [ "buildInputs" ];
 in
 stdenv.mkDerivation ({
-  buildInputs = [ mxbuildPkg nodejs ] ++ args.buildInputs or [];
+  buildInputs = [ mxbuildPkg nodejs ] ++ buildInputs;
   installPhase = ''
     mkdir -p $out
     mxbuild --target=package --output=$out/${name}.mda --java-home ${jdk} --java-exe-path ${jdk}/bin/java ${stdenv.lib.optionalString looseVersionCheck "--loose-version-check"} "$(echo *.mpr)"
     mkdir -p $out/nix-support
     echo "file binary-dist \"$(echo $out/*.mda)\"" > $out/nix-support/hydra-build-products
   '';
-} // args)
+} // extraArgs)

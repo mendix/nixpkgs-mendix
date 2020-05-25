50702f5ec2773465a1e86bab92d72acbf84328af
Asko Soukka <asko.soukka@iki.fi>
2020-05-25 20:54:10 +0300

    Add support for Mendix 8

--- top-level/all-packages.nix
+++ top-level/all-packages.nix
@@ -12,6 +12,9 @@ let
       "7.17.2" = callPackage ../pkgs/mendix/7.17.2.nix { };
       "7.20.0.41900" = callPackage ../pkgs/mendix/7.20.0.41900.nix { };
       "7.22.2.44474" = callPackage ../pkgs/mendix/7.22.2.44474.nix { };
+      "8.9.0.5487" = callPackage ../pkgs/mendix/8.9.0.5487.nix {
+        jre = pkgs.openjdk11;
+      };
     };
 
     mxbuild = {
@@ -29,6 +32,9 @@ let
       };
       "7.22.2.44474" = callPackage ../pkgs/mxbuild/7.22.2.44474.nix {
         mono = pkgs.mono46;
+      };
+      "8.9.0.5487" = callPackage ../pkgs/mxbuild/8.9.0.5487.nix {
+        mono = pkgs.mono5;
       };
     };
 
